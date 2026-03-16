a4de28a8dc94770cfd9cc1e769c6b7282983a68d
Roganci Fontelera <roganci03@gmail.com>
2026-03-16 13:26:09 -0700

    actually removing the comments from prep_data.py

--- data/prep_data.py
+++ data/prep_data.py
@@ -1,18 +1,5 @@
 import duckdb
 
-# duckdb.execute("""
-#     COPY (SELECT * FROM read_csv_auto('raw/Students-Social-Media-Addiction.csv'))
-#     TO 'processed/Students-Social-Media-Addiction.parquet' (FORMAT PARQUET)
-# """)
-
-# duckdb.execute(f"""
-#     COPY (
-#         SELECT *,
-#         CASE WHEN "Affects_Academic_Performance" = true THEN 'Yes' ELSE 'No' END AS "Affects_Academic_Performance"
-#         FROM read_csv_auto('raw/Students-Social-Media-Addiction.csv')
-#     )
-#     TO 'processed/Students-Social-Media-Addiction.parquet' (FORMAT PARQUET)
-# """)
 duckdb.execute(f"""
     COPY (
         SELECT 
