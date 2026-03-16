7da0bd8be94ad60d781eaebe67dcd1a9be7b718d
Roganci Fontelera <roganci03@gmail.com>
2026-03-16 13:24:18 -0700

    Refined query for the parquet

--- data/prep_data.py
+++ data/prep_data.py
@@ -1,6 +1,24 @@
 import duckdb
 
-duckdb.execute("""
-    COPY (SELECT * FROM read_csv_auto('raw/Students-Social-Media-Addiction.csv'))
+# duckdb.execute("""
+#     COPY (SELECT * FROM read_csv_auto('raw/Students-Social-Media-Addiction.csv'))
+#     TO 'processed/Students-Social-Media-Addiction.parquet' (FORMAT PARQUET)
+# """)
+
+# duckdb.execute(f"""
+#     COPY (
+#         SELECT *,
+#         CASE WHEN "Affects_Academic_Performance" = true THEN 'Yes' ELSE 'No' END AS "Affects_Academic_Performance"
+#         FROM read_csv_auto('raw/Students-Social-Media-Addiction.csv')
+#     )
+#     TO 'processed/Students-Social-Media-Addiction.parquet' (FORMAT PARQUET)
+# """)
+duckdb.execute(f"""
+    COPY (
+        SELECT 
+            * EXCLUDE ("Affects_Academic_Performance"),
+            CASE WHEN "Affects_Academic_Performance" = true THEN 'Yes' ELSE 'No' END AS "Affects_Academic_Performance"
+        FROM read_csv_auto('raw/Students-Social-Media-Addiction.csv')
+    )
     TO 'processed/Students-Social-Media-Addiction.parquet' (FORMAT PARQUET)
 """)
